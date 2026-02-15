5c116d008e3aac79cbf8f5f6455a1ef926c35ee4
tanishk kushwaha <149100136+droid85real@users.noreply.github.com>
2026-02-15 08:42:19 +0000

    gsap navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,35 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { navLinks } from "../../constants";
 
 const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top", //bottom of the nav touches the top of the viewport -> animation
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(20px)",
+        duration: 1,
+        ease: "power1.inOut",
+      },
+    );
+    
+  });
+
   return (
     <nav>
       <div>
         <a href="#home" className="flex items-center gap-2">
-            <img src="/images/logo.png" alt="logo" />
-            
+          <img src="/images/logo.png" alt="logo" />
+
           <p>Velvet Pour</p>
         </a>
       </div>
