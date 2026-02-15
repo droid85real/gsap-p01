0462f7023e2e30533c52913fce9620fbd38e2a20
tanishk kushwaha <149100136+droid85real@users.noreply.github.com>
2026-02-15 16:41:06 +0000

    implemented the about section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { SplitText } from "gsap/SplitText";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
+import About from "./components/About";
 
 // makes the plugin available to GSAP animations
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -14,6 +15,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
+      <About />
     </main>
   );
 };
