8403bfdcdf1bbd2f137b224dfcf4a5e9b64c1238
tanishk kushwaha <149100136+droid85real@users.noreply.github.com>
2026-02-15 10:07:01 +0000

    hero page gsap

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 // makes the plugin available to GSAP animations
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -10,6 +11,8 @@ const App = () => {
   return (
     <main>
       <Navbar />
+      <Hero />
+      <div className="h-dvh bg-black" /> {/* temporary */}
     </main>
   );
 };
